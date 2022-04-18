910950b69b2d29c3caa3ae7219b7abe38b8b911b
Andresser17 <tensasangetsu1254@gmail.com>
2022-04-18 15:32:32 -0400

    test: verified behavior when modifying an existing exchange rate

--- js/test_money.js
+++ js/test_money.js
@@ -4,7 +4,7 @@ const Portfolio = require("./portfolio");
 const Bank = require("./bank");
 
 class MoneyTest {
-	constructor() {
+	setUp() {
 		this.bank = new Bank();
 		this.bank.addExchangeRate("EUR", "USD", 1.2);
 		this.bank.addExchangeRate("USD", "KRW", 1100);
@@ -67,11 +67,17 @@ class MoneyTest {
 		}, expectedError);
 	}
 
-	testConversion() {
+	testConversionWithDifferentRatesBetweenTwoCurrencies() {
 		let bank = new Bank();
 		bank.addExchangeRate("EUR", "USD", 1.2);
 		let tenEuros = new Money(10, "EUR");
 		assert.deepStrictEqual(bank.convert(tenEuros, "USD"), new Money(12, "USD"));
+
+		this.bank.addExchangeRate("EUR", "USD", 1.3);
+		assert.deepStrictEqual(
+			this.bank.convert(tenEuros, "USD"),
+			new Money(13, "USD")
+		);
 	}
 
 	testConversionWithMissingExchangeRate() {
@@ -92,12 +98,22 @@ class MoneyTest {
 		return testMethods;
 	}
 
+	randomizeTestOrder(testMethods) {
+		for (let i = testMethods.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[testMethods[i], testMethods[j]] = [testMethods[j], testMethods[i]];
+		}
+
+		return testMethods;
+	}
+
 	runAllTests() {
-		let testMethods = this.getAllTestMethods();
+		let testMethods = this.randomizeTestOrder([...this.getAllTestMethods()]);
 		testMethods.forEach((m) => {
 			console.log(`Running: ${m}()`);
 			let method = Reflect.get(this, m);
 			try {
+				this.setUp();
 				Reflect.apply(method, this, []);
 			} catch (e) {
 				if (e instanceof assert.AssertionError) {
